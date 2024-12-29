4d28efc356374dc491ed60a6737e1e4deba43219
mhdine02 <younes.moh.fst@uhp.ac.ma>
2024-12-29 12:10:53 +0100

    Message décrivant les changements

--- MyclassTest.java
+++
@@ -1,7 +0,0 @@
-
-import static org.junit.Assert.*;
-public class MyclassTest extends TestCase {
-    public void testCalculer() throws Exception{
-        assertEquals(2,Myclass.calculer )
-    }
-}
